ff515517af818cb26fc446fe166e8b456050e360
Hugo M <hugo@hugomassarri.com>
2018-08-21 22:23:37 -0400

    cleaning up files

--- index.php
+++ index.php
@@ -1,54 +1,33 @@
-<<<<<<< HEAD
-<?php include 'includes/header.php'; ?>
-<body>
-    <!--[if lte IE 9]>
-    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-    <![endif]-->
-
-    <!-- //Add your site or application content here -->
-   <header>
-        <div class="navbar-fixed">
-
-            <nav class="z-depth-0 white">
-                <div class="nav-wrapper container">
-                    <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons black-text">menu</i></a>
-                    <h1 class="black-text"></h1>
-                </div>
-            </nav>
-            <?php include 'includes/sidenav.php'; ?>
-        </div>
-    </header>
-    <main>
-        <div style="height: 10vh;"></div>
-=======
 <!doctype html>
 <html class="no-js" lang="">
-<?php include 'includes/header.php'; ?>
-    <body>
-    <?php include 'includes/top_side_nav.php'; ?>
+<?php include 'includes/head.php'; ?>
 
-    <main>
-        
-    <!--
-        <div class="primary-dark" style="height: 40vh;"></div>
->>>>>>> 9cf20aac68739110f6a558b6a74f53dcdd0463a9
-        <div id="slogan" class="row">
-            <div class="col s12 center-align">
-                <img class="responsive-img" src="images/slogan.png" alt="" />
-            </div>
-        </div>
-    -->
-    </main>
-<<<<<<< HEAD
-<?php include 'includes/footer.php'; ?>
-<?php include 'includes/js.php'; ?>
+<body>
+	<header>
+		<div class="navbar-fixed">
+			<nav class="z-depth-0 white">
+				<div class="nav-wrapper container">
+					<a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons black-text">menu</i></a>
+					<h1 class="black-text">Test Title</h1>
+				</div>
+			</nav>
+			<?php include 'includes/sidenav.php'; ?>
+		</div>
+	</header>
+
+	<main>
+		<div style="height: 10vh;"></div>
+		<div id="slogan" class="row">
+			<div class="col s12 center-align">
+				<img class="responsive-img" src="images/slogan.png" alt="" />
+			</div>
+		</div>
+	</main>
+
+
+
+	<?php include 'includes/footer.php'; ?>
+	<?php include 'includes/js.php'; ?>
 </body>
-</html>
-=======
 
-
-    <?php include 'includes/footer.php'; ?>
-    <?php include 'includes/js.php'; ?>
-    </body>
 </html>
->>>>>>> 9cf20aac68739110f6a558b6a74f53dcdd0463a9
