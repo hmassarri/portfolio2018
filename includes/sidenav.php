99751b53311e13d8fb022f7d0eb1771c2b66327a
Hugo M <hugo@hugomassarri.com>
2018-08-23 00:05:50 -0400

    added process, created more css

--- includes/sidenav.php
+++ includes/sidenav.php
@@ -12,11 +12,11 @@
                     </div>
                 </li>
                 <li><a href="uix.php" class="link black-text waves-effect waves-light">UIX</a></li>
-                <li><a href="#" class="link black-text waves-effect waves-light">Process</a></li>
-                <li><a href="#" class="link black-text waves-effect waves-light">Blog</a></li>
-                <li><a href="#" class="link black-text waves-effect waves-light">Discography</a></li>
-                <li><a href="#" class="link black-text waves-effect waves-light">Art</a></li>
-                <li><a href="#" class="link black-text waves-effect waves-light">Games</a></li>
-                <li><a href="#" class="link black-text waves-effect waves-light">Resume</a></li>
-                <li><a href="#" class="link black-text waves-effect waves-light">Contact</a></li>
+                <li><a href="process.php" class="link black-text waves-effect waves-light">Process</a></li>
+                <li><a href="blog.php" class="link black-text waves-effect waves-light">Blog</a></li>
+                <li><a href="discography.php" class="link black-text waves-effect waves-light">Discography</a></li>
+                <li><a href="art.php" class="link black-text waves-effect waves-light">Art</a></li>
+                <li><a href="games.php" class="link black-text waves-effect waves-light">Games</a></li>
+                <li><a href="resume.php" class="link black-text waves-effect waves-light">Resume</a></li>
+                <li><a href="contact.php" class="link black-text waves-effect waves-light">Contact</a></li>
             </ul>
---
+++ process.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html class="no-js" lang="">
+<?php include 'includes/head.php'; ?>
+
+<body>
+    <!--[if lte IE 9]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+    <![endif]-->
+
+    <!-- //Start -->
+   <header>
+        <div class="navbar-fixed">
+
+            <nav class="top-nav z-depth-0 white">
+                <div class="nav-wrapper container">
+                    <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons black-text">menu</i></a>
+                    <h1 class="black-text">Process</h1>
+                    <h5>UX methods used to surface usability and create a great experience</h5>
+                </div>
+            </nav>
+            <?php include 'includes/sidenav.php'; ?>
+        </div>
+    </header>
+    
+    
+    <!-- // Add site content here -->
+    <main>
+		<div id="processContainer" class="container">
+			<div class="row">
+				<div class="col s12">
+					<h3>Mental Model</h3>
+					<p>Capture user behavior, pain points, motivations, and needs from contextual inquiries.  Group into logical pillars.  Map out product features for each pillar to identify gaps, opportunities, and feature bloat.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12">
+					<h3>Personas</h3>
+					<p>Summarize the research data from your mental model into personas.  This will help your team keep the user’s motivations, frustrrations, and goals in mind when evaluate design solutions.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12">
+					<h3>Customer Journey Map</h3>
+					<p>After you have identified who will be using your product, write down the business goals/desired outcome for each persona.  You then go back and fill in how they will discover your product, learn what it is all about, and how they will start using it</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12">
+					<h3>Task Analysis</h3>
+					<p>Identifies user’s thought process and desired reactions toward each task flow segment</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12">
+					<h3>Red Route Analysis</h3>
+					<p>Prioritize user experience for common tasks.  Tasks used by few and rarely can be harder for user to access so that tasks used frequently by most people can be easily accomplished.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12">
+					<h3>Storyboards</h3>
+					<p>Visually depict scenarios with storyboards.  The storyboard should show the context, the problem, the user’s fears/motiviations, and how the problem will be solved.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12">
+					<h3>Wireframes</h3>
+					<p>Visually communicate abstract concepts with users and team members to generate feedback.  These can be drawn by hand or with software like Sketch, Balsamiq, Axure, or Omnigraffle</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12">
+					<h3>Rapid Prototyping</h3>
+					<p>Get users involved early in the design process with quick and inexpensive paper prototypes.  Uncover usability issues and validate design decisions based on acutal user reactions</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12">
+					<h3>Style Tiles</h3>
+					<p>Jumpstart the visual design language even before the interaction designs are finalized.  Start with common UI elements or components that would be found in the product your are building and iterate through several varations</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12">
+					<h3>High Fidelity Mockups and Prototypes</h3>
+					<p>Apply product branding, grid structure, typography, color scheme, and icons.<br />
+					Develop interactive prototypes with tools like Sketch or Invision.  This lets the team easily observe exactly how well the product will be received before the product gets built.  It gives the team a chance to see and take time to identify any roadblocks that haven’t been surfaced yet</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s12">
+					<h3>Pattern Library</h3>
+					<p>Create a reusable pattern library in an app like Sketch so that you  and your team member do not have to keep recreating commonly used controls over and over again in your designs</p>
+				</div>
+			</div>
+		</div>
+    </main>
+<?php include 'includes/footer.php'; ?>
+<?php include 'includes/js.php'; ?>
+</body>
+</html>
