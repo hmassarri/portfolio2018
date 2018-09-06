24156b199f01deb0b2773968d8b62cc5cee53f72
Hugo M <hugo@hugomassarri.com>
2018-09-06 01:23:40 -0400

    removed old build

--- includes/footer.php
+++
@@ -1,9 +0,0 @@
-	<footer class="page-footer white">
-        <div class="footer-copyright white">
-            <div class="container">
-                <div class="row">
-                    <div class="col s12 center-align white black-text">&COPY; 2000-2018 HUGO M. / HUGOMASSARRI.COM / All Rights Reserved</div>
-                </div>
-            </div>
-        </div>
-    </footer>
